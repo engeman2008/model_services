45ac95d2934a47519bc2aa9f452d7e9dc4f1878d
Eman <eman.cse2008@gmail.com>
2021-06-24 10:11:42 +0200

    save state

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -14,14 +14,43 @@ class ModelService {
     }
   }
 
-  public async saveModel(modelData: any): Promise<MyModelDoc> {
+  public async createModel(modelData: any): Promise<MyModelDoc> {
     try {
       const myModel = new MyModel(modelData);
-      await myModel.save();
+      await myModel.save((err: any, data: any) => {
+        if (err) {
+          throw new HttpException(404, err);
+        }
+        return data;
+      });
       return myModel;
     } catch (error) {
-      throw new HttpException(422, 'Failed to add');
+      throw new HttpException(422, 'Failed to create');
     }
+  }
+
+  public async updateModel(modelId: string, modelData: any) {
+    await this.findModelById(modelId);
+
+    try {
+      const myModel = new MyModel(modelData);
+      await myModel.save((err: any, data: any) => {
+        if (err) {
+          throw new HttpException(404, err);
+        }
+        return data;
+      });
+      return myModel;
+    } catch (error) {
+      throw new HttpException(422, 'Failed to update');
+    }
+  }
+
+  private async doesModelExists(modelId: string) {
+    await MyModel.findById(modelId, (err: any, data: any) => {
+      if (err) return false;
+      return data;
+    });
   }
 }
 export default ModelService;
